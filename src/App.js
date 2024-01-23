033ec5222dec2ec6cd72d444eb4c27c1c0b1111c
kylevanbibber <108955710+kylevanbibber@users.noreply.github.com>
2024-01-23 12:19:03 -0500

    new

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
-import React, { useEffect } from 'react';
+// App.js
+import React from 'react';
 import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
 import Production from './components/Production';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import AuthHOC from './AuthHOC'; // Import the AuthHOC
+import ExternalLoginRedirect from './components/ExternalLoginRedirect'; // Import the ExternalLoginRedirect component
 function App() {
-    useEffect(() => {
-        // Check if the user is authenticated
-        const jwtToken = localStorage.getItem('jwtToken');
-
-        if (!jwtToken) {
-            // User is not authenticated, redirect to the login page
-            window.location.href = 'https://ariaslife.com/agents/login.html'; // Replace with your login URL
-        }
-        // Add any other conditions or handling as needed
-
-    }, []);
-
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" exact element={<Dashboard />} />
-                <Route path="/production" element={<Production />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={AuthHOC(Dashboard)} /> {/* Protected route */}
+        <Route path="/production" element={AuthHOC(Production)} /> {/* Protected route */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
