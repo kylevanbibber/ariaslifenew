9362711a456ad319ccc423a4929fdf54d11f4a27
kylevanbibber <108955710+kylevanbibber@users.noreply.github.com>
2024-01-23 12:20:52 -0500

    new

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React from 'react';
 import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
@@ -6,18 +5,17 @@ import Production from './components/Production';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AuthHOC from './AuthHOC'; // Import the AuthHOC
-import ExternalLoginRedirect from './components/ExternalLoginRedirect'; // Import the ExternalLoginRedirect component
+
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={AuthHOC(Dashboard)} /> {/* Protected route */}
-        <Route path="/production" element={AuthHOC(Production)} /> {/* Protected route */}
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" exact element={<Dashboard />} />
+                <Route path="/production" element={<Production />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
